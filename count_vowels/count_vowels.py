faaf8ff2547ba921e03704c1ab086f2d6f9514aa
Attila Sasvari <attila.sasvari@kyndryl.com>
2022-10-12 10:05:49 +0200

    simplify function

--- count_vowels/count_vowels.py
+++ count_vowels/count_vowels.py
@@ -2,8 +2,6 @@ def count_vowels(word: str) -> int:
     """Return number of vowels in given word."""
     VOWELS = "aeiou"
 
-    letters = [w for w in word]
-
-    vowel_count = len([l for l in letters if l in VOWELS])
+    vowel_count = len([l for l in list(word) if l in VOWELS])
 
     return vowel_count
